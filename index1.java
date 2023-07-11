a85a798fa3b36d69fdb6eddf33e70ea786e85e6e
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-11 16:57:28 +0700

    Update index1.java

--- index1.java
+++ index1.java
@@ -1,5 +1,19 @@
-class index1 {
+import java.util.Scanner;
+
+public class index1 {
   public static void main(String[] args) {
-    System.out.println("Namstey Duniya ");
+    System.out.println("Namstey Duniya ");  // print the output in java
+    
+    Scanner sc = new Scanner (System.in);
+     System.out.println(sc.nextLine());    // to take string input 
+
+    // primitive data type are which cannot be broken to more simple data 
+    // e.g :-
+    // int a = 4 ;
+    // char name = 'b';
+    // float marks = 123.7f;
+    // double largeDecimalNumber = 1234.456;
+    // long largeIntValue = 123445L;
+    // bool yet = true;
   }
 }
