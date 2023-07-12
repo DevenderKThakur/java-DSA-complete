801ac1e8e15322e092b34a5cc07740c1d50feba5
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-12 17:48:14 +0700

    Update conditionloop.java

--- conditionloop.java
+++ conditionloop.java
@@ -2,5 +2,12 @@ import java.util.Scanner ;
 class conditionloop {
   public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
-   
+    
+   // calculate factorial of a number 
+    int num1 = sc.nextInt();
+    int f = 1;
+    for(int i = 1;i<=num1;i++){
+      f=f*i;
+    }
+    System.out.println(f);
 }
