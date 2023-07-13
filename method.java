c16dce4f2227499230e68cc014049d65d7822082
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-13 16:53:10 +0700

    Update method.java

--- method.java
+++ method.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 public class method{
     public static void main(String[] args) {
        Scanner sc = new Scanner (System.in);
-       System.out.println("enter the first ");
-       int a = sc.nextInt();
-       System.out.println("enter the second ");
-       int b = sc.nextInt();
-       System.out.println("enter the third  ");
-       int c = sc.nextInt();
-       int largest = largest(a, b, c);
-       System.out.println("largest number  "+largest);
-       int low  = smallest(a, b, c);
-       System.out.println("smallest number "+low);
+       // System.out.println("enter the first ");
+       // int a = sc.nextInt();
+       // System.out.println("enter the second ");
+       // int b = sc.nextInt();
+       // System.out.println("enter the third  ");
+       // int c = sc.nextInt();
+       // int largest = largest(a, b, c);
+       // System.out.println("largest number  "+largest);
+       // int low  = smallest(a, b, c);
+       // System.out.println("smallest number "+low);
+
+        // int n = sc.nextInt();
+        // oddEven(n);
     }
     public static int largest (int a , int b , int c ){
         int max = a;
@@ -32,6 +35,14 @@ public class method{
             small = c;
         }
         return small;  
+    }
+    // to find the number is even or odd
+       static void oddEven(int n ){
+        if(n%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("odd");
+        }
     }
    
 }
