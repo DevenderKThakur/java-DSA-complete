b2a3f2691606ca9e4ecbc941efb07f75b2fdc7ed
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-13 22:35:40 +0700

    Update array.java

--- array.java
+++ array.java
@@ -3,15 +3,23 @@ public class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         // declaration of an array 
-        int[] arr = new int[5];
+        // int[] arr = new int[5];
 
         // take input and store the value in array 
-        for(int i = 0;i<arr.length;i++){
-            arr[i] = sc.nextInt();
-        }
+        // for(int i = 0;i<arr.length;i++){
+        //     arr[i] = sc.nextInt();
+        // }
         // print the array 
-        for (int i = 0;i<arr.length ;i++){
-            System.out.println(arr[i]);
-        }
+        // for (int i = 0;i<arr.length ;i++){
+        //     System.out.println(arr[i]);
+        // }
+        // change the value of the first index of the array 
+        int[] num = {3,4,5,6,7};
+        System.out.println(num[0]);
+        change(num);
+        System.out.println(num[0]);
+    }
+     public static void change(int[] arr1){
+        arr1[0] = 99; 
     }
 }
