22a7de21e5d1fb4f7c0ed480d270b45b1b738d19
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-13 22:45:00 +0700

    Update array.java

--- array.java
+++ array.java
@@ -13,11 +13,29 @@ public class array {
         // for (int i = 0;i<arr.length ;i++){
         //     System.out.println(arr[i]);
         // }
+        
         // change the value of the first index of the array 
-        int[] num = {3,4,5,6,7};
-        System.out.println(num[0]);
-        change(num);
-        System.out.println(num[0]);
+        // int[] num = {3,4,5,6,7};
+        // System.out.println(num[0]);
+        // change(num);
+        // System.out.println(num[0]);
+
+        //  2d array 
+        int[][] arr = new int[3][3];
+
+        // take input in the array 
+        for (int i =0;i<3;i++){
+            for(int j = 0;j<3;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        // print the array 
+         for (int i =0;i<3;i++){
+            for(int j = 0;j<3;j++){
+              System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
     }
      public static void change(int[] arr1){
         arr1[0] = 99; 
