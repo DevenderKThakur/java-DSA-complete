314a814161dcae042a43774dc2486107c64dff48
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-14 14:09:22 +0700

    Update arrayQuestion.java

--- arrayQuestion.java
+++ arrayQuestion.java
@@ -6,10 +6,13 @@ public class arrayQuestion{
         // swap(arr, 1,3);
         // System.out.println(Arrays.toString(arr)
 
-         // System.out.println(max(arr,0,4));  // to find max val
+         // System.out.println(max(arr,0,4));                 // to find max val
 
         //  rev(arr);
         // System.out.println(Arrays.toString(arr));
+
+        // int[] nums = {1,2,1};                              //leetcode 1929
+        // System.out.println(concatenation(nums));
   }
    static void swap(int[] arr , int i , int j ){   // swap the value in array method 
         int t = arr[i];
@@ -33,5 +36,21 @@ public class arrayQuestion{
             start++;
             end--;
          }
+    }
+    static String  buildArray(int[] nums){   // leetcode 1920
+        int[] ans = new int[nums.length] ;
+        for(int i =0;i<nums.length;i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return Arrays.toString(ans);
+    }
+   static String concatenation(int[] nums ){ // leetcode 1929
+        int n = nums.length;
+        int[] ans = new int[n*2];
+        for(int i = 0;i<n;i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return Arrays.toString(ans);
     }
 }
