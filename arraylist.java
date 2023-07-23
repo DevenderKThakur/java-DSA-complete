21145fae8b612a434db04d2a6edcb9cac9f519e7
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-23 20:08:48 +0700

    Update arraylist.java

--- arraylist.java
+++ arraylist.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 public class arraylist {
     public static void main(String[] args) {
         ArrayList<Integer> mynum = new ArrayList<Integer>();
@@ -6,5 +7,12 @@ public class arraylist {
         mynum.add(20);
         mynum.add(30);
         System.out.println(mynum);
+        int element =  mynum.get(0); 
+         System.out.println(element);
+        // mynum.remove(0);   to remove the element in the array 
+        Collections.sort(mynum);
+        for(int i:mynum){
+            System.out.println(i);
+        }
     }
 }
