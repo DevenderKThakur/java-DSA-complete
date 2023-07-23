385e0c06241c4a5ddd82d1c29107d47645c84654
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-23 18:29:50 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> mynum = new ArrayList<Integer>();
+        mynum.add(10);
+        mynum.add(20);
+        mynum.add(30);
+        System.out.println(mynum);
+    }
+}
