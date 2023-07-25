f5f009efadde11239da36215c43564c6aaf70d48
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-25 13:11:48 +0700

    Update binarySearch.java

--- binarySearch.java
+++ binarySearch.java
@@ -51,4 +51,25 @@ public class binarySearch {
         }
         return - 1;
     }
+     // c = smallest element in array >= target
+    static int ceiling(int [] arr , int target ){
+        if(target > arr[arr.length -1 ]){
+            return - 1;
+        }
+        int start = 0;
+        int end = arr.length - 1 ;
+        while (start <= end ){
+            int mid = start + (end - start)/2;
+            if(target < arr[mid]){
+               end = mid - 1 ;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                return mid ;
+            }
+        }
+        return start ;
+    }
 }
