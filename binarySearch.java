28c2625b153ccace7fa5ff68d1f219b26f243fce
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-25 13:12:20 +0700

    Update binarySearch.java

--- binarySearch.java
+++ binarySearch.java
@@ -72,4 +72,22 @@ public class binarySearch {
         }
         return start ;
     }
+    //  c = greatest element in array <= target 
+    static int floorSearch(int [] arr , int target ){
+        int start = 0;
+        int end = arr.length - 1 ;
+        while (start <= end ){
+            int mid = start + (end - start)/2;
+            if(target < arr[mid]){
+               end = mid - 1 ;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                return mid ;
+            }
+        }
+        return end  ; 
+    }
 }
