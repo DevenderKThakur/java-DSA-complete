489acfd5a6e190e978ef738cc3cbaa8d470823c0
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-25 17:17:07 +0700

    Update binarySearch.java

--- binarySearch.java
+++ binarySearch.java
@@ -90,4 +90,19 @@ public class binarySearch {
         }
         return end  ; 
     }
+     //  char question 
+    static char nextLetters(char [] letters , char target ){
+        int start = 0;
+        int end = letters.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target < letters[mid]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return letters[start%letters.length];
+    }
 }
