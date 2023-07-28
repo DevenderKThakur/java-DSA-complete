652b55efc2dd00520202a0529a27329d1f95c519
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-07-28 16:00:42 +0700

    Update sorting.java

--- sorting.java
+++ sorting.java
@@ -43,4 +43,15 @@ public class sorting {
             arr[j+1] = current;
         }
     }
+    //  cycle sort O(n)
+    static void cyclesort(int [] arr ){
+        for(int i=0;i<arr.length;i++){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                int t = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = t ;
+            }
+        }
+    }
 }
