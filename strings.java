fe5449e2a53d5a65fe085c9b6e6d68d4849c66ee
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-08-07 13:33:40 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String name = sc.next(); // takes the string as input until the space 
+        String name2 = sc.next();
+        System.out.println(name + "  " +name2);
+    }
+}
